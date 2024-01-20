90a7946f8239d83c2e746a3c4b4f129bffb6beaf
OPauloChagas <pmanu.chagas@gmail.com>
2024-01-20 14:16:25 -0300

    Projeto chat estudo SignalR

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstudoIntroSignaIR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
